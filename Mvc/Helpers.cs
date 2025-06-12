6f283c351007be8c7c25473ae9168870c5764f7b
Anders Abel <anders.abel@sustainsys.com>
2025-06-12 14:49:41 +0200

    Update certificate thumbprint

--- Mvc/Helpers.cs
+++ Mvc/Helpers.cs
@@ -7,7 +7,7 @@ public static class Helpers
     // This thumbprint works on my machine<tm>. To get it running on your
     // machine, open certificate manager (mmc.exe, add certificates, current user)
     // and find a thumbprint of any of your personal certificates.
-    public const string Thumbprint = "b7da225ae3ed72956e40e1777a63db114bdc545a";
+    public const string Thumbprint = "e612a25a9617aef04f3795380cf46425ef680451";
 
     public static void ConfigureIdentityServer(this OpenIdConnectOptions opt)
     {
